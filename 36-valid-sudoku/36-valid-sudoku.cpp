5d8e50dbfed2e3deee5d51090a2b9f36a49c1b44
Mohamed-ahmed-shokry <65525899+Mohamed-ahmed-shokry@users.noreply.github.com>
2022-08-01 21:38:29 +0200

    Time: 76 ms (5.25%), Space: 22.2 MB (5.17%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board)
+    {
+        int n = board.size();
+        unordered_map<string, int> seen;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int element = board[i][j];
+                
+                if(element == '.')
+                    continue;
+                
+                string rowFind = to_string(element) + "found at row" + to_string(i);
+                
+                string colFind = to_string(element) + "found at column" + to_string(j);
+                
+                string boxFind = to_string(element) + "found at box" + to_string(i/3) + '-' + to_string(j/3);
+
+                if(seen.find(rowFind) != seen.end() ||
+                   seen.find(colFind) != seen.end() ||
+                   seen.find(boxFind) != seen.end()){
+                    return false;
+                }
+                
+               seen[rowFind]++;
+               seen[colFind]++;
+               seen[boxFind]++;
+            
+            }
+        }
+        return true;
+        
+    }
+};
