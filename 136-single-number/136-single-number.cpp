006efdef0d1d610c64e159292eb5ee9e664a4fc8
Mohamed-ahmed-shokry <65525899+Mohamed-ahmed-shokry@users.noreply.github.com>
2022-09-06 23:14:41 +0200

    Time: 72 ms (6.14%), Space: 20 MB (18.78%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        unordered_map<int, int> freq;
+        
+        for (int num:nums)
+            ++freq[num];
+        
+        for (auto pair:freq)
+            if(pair.second == 1)
+                return pair.first;
+        
+        return 0;
+    }
+};
