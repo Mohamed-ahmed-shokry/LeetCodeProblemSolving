e8bfa721860fe8db450228f15d2e40687bb007b4
Mohamed-ahmed-shokry <65525899+Mohamed-ahmed-shokry@users.noreply.github.com>
2022-09-16 22:23:34 +0200

    Time: 15 ms (6.90%), Space: 9.3 MB (27.23%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,7 +5,7 @@ public:
     {
         string key = to_string(m) + "," + to_string(n);
         if(myMap[key]) return myMap[key];
-        if (m == 1 || m == 1) return 1;
+        if (m == 1 || n == 1) return 1;
         if(m == 0 || n == 0) return 0;
         
         myMap[key] = uniquePaths(m-1, n) + uniquePaths(m,n-1);
